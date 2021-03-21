70638a3073db82610cdf14ffba3f11e06e35ca34
Keighley McFarland <mcfarland.ke@gmail.com>
2021-03-21 16:25:46 +0100

    binding practice

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TestabilityRegistry } from '@angular/core';
 
 @Component({
-  selector: 'app-servers', // for components convention is to select by component. other optionsselect by attribute "[]", select by class ".class"
+  selector: 'app-servers', // for components convention is to select by component. other optionsselect by attribute "[]", event (), select by class ".class"
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  allowNewServer = false
+  allowNewServer: boolean = false
+  serverCreationStatus: string = "create new server"
+  serverName = "test"
 
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true
     }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "server was created"
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value
   }
 
   ngOnInit(): void {
